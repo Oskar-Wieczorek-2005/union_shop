fcfcfe166492d0e7e739578b020e5617e1495390
Oskar Wieczorek <oziw57@gmail.com>
2025-11-17 19:46:19 +0000

    impliment new footer in the product_page

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -251,20 +251,8 @@ class ProductPage extends StatelessWidget {
               ),
             ),
 
-            // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+            // Footer imported from main.dart
+            const Footer(),
           ],
         ),
       ),
