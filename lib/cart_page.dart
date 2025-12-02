d109a5123e6a911104b146189598b6a708042619
Oskar Wieczorek <oziw57@gmail.com>
2025-12-02 10:46:17 +0000

    add cart title, and text for when it is empty

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -10,6 +10,8 @@ class CartPage extends StatelessWidget {
       Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
     }
 
+    bool isCartEmpty = true;
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -18,6 +20,23 @@ class CartPage extends StatelessWidget {
               onHomeTap: navigateToHome,
               onPlaceholderTap: () {},
             ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+              child: Center(
+                child: Text(
+                  'Your Cart',
+                ),
+              ),
+            ),
+            if (isCartEmpty)
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Center(
+                  child: Text(
+                    'Your cart is currently empty',
+                  ),
+                ),
+              ),
             const Footer(),
           ],
         ),
